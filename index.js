8b3a7ad3baef4adc577a43d1d65613c10b6bbcc8
Jay <108143448+godlyjenna@users.noreply.github.com>
2024-06-18 21:45:59 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const addBtn = document.getElementById('btn');
+const respond = document.getElementById('response');
+
+
+
+
+//Add if statement: if name, amount, date is entered write: 
+function myButton() {
+    respond.innerHTML = "Transaction has been accepted!";
+    respond.style.color="lime";
+}
+// else: respond.innerHTML = "Transaction could not be accepted.", respond.style.color="orangered";
